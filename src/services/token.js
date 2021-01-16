84ed860562ff817446c68c479af00caad4ea270f
Maick Souza <maick_a_s@msn.com>
2021-01-16 12:53:06 -0300

    chore(server): configure jwt token #1

--- src/services/token.js
+++ src/services/token.js
@@ -1,6 +1,14 @@
-const sign = _ => "Generated token"
+const jwt = require('jsonwebtoken')
 
-const verify = _ => new Promise((resolve, reject) => resolve(true))
+const sign = payload => jwt.sign(payload, 'rocketseat')
+
+const verify = token => new Promise((resolve, reject) =>
+  jwt.verify(
+    token,
+    'rocketseat',
+    (error, data) => error ? reject(error) : resolve(data)
+  )
+)
 
 module.exports = {
   sign,
